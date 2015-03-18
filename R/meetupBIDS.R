8ee7335178f67619657ec629c3a8609eb7b9b628
Dani Ushizima <dani.lbnl@gmail.com>
2015-03-18 08:50:31 -0700

    More tips if you get stuck

--- R/meetupBIDS.R
+++ R/meetupBIDS.R
@@ -1,8 +1,11 @@
 #' Hello world
 #' 
 #' Super smart example
-#' Check Hadley's book to learn more
+#' Check Hadley's book to learn more:
 #' http://r-pkgs.had.co.nz/
+#' 
+#' Got stuck? Check http://hilaryparker.com/2014/04/29/writing-an-r-package-from-scratch/
+#' 
 #' @param who Optional string of addition stuff for function to do
 #' @return string that was printed
 #' @examples
